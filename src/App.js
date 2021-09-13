a8708a7354b30b80921fa6f72c7d39dc0ad2a4f9
Roddyd94 <pky941129@gmail.com>
2021-09-13 09:24:16 +0900

    hello fin

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
-import IterationSample from './IterationSample';
-import LifeCycleSample from './LifeCycleSample';
+import Counter2 from './Counter2';
 
-function getRandomColor() {
-  return '#' + Math.floor(Math.random() * 16777215).toString(16);
-}
+// function getRandomColor() {
+//   return '#' + Math.floor(Math.random() * 16777215).toString(16);
+// }
 
 class App extends Component {
-  state = {
-    color: '#000000',
-  };
+  // state = {
+  //   color: '#000000',
+  // };
 
-  handleClick = () => {
-    this.setState({
-      color: getRandomColor(),
-    });
-  };
+  // handleClick = () => {
+  //   this.setState({
+  //     color: getRandomColor(),
+  //   });
+  // };
 
   render() {
     return (
       <div>
-        <button onClick={this.handleClick}>랜덤 색상</button>
-        <LifeCycleSample color={this.state.color} />
+        {/* <button onClick={this.handleClick}>랜덤 색상</button> */}
+        {/* <ErrorBoundary> */}
+        {/* <LifeCycleSample color={this.state.color} /> */}
+        {/* </ErrorBoundary> */}
+        <Counter2 />
       </div>
     );
   }
